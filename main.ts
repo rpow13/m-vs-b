7b0f3e761443a3918393cf2d1c17ad5dab4612b4
rpow13 <45049326+rpow13@users.noreply.github.com>
2022-11-28 19:18:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showString("M VS B!!")
+for (let index = 0; index <= 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    basic.showNumber(3 - index)
+}
+basic.showString("GO")
+basic.forever(function () {
+    for (let index = 0; index <= 4; index++) {
+    	
+    }
+})
